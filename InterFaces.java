91461c6e9e49076e9ab62035d74791d6d85129ea
Howie Wu <69863727+hwu5@users.noreply.github.com>
2022-01-27 01:29:44 +0000

    Make delete button2

--- InterFaces.java
+++ InterFaces.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
 import java.util.Random;
 
